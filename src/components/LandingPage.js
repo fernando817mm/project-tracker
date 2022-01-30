48ae883c1e09e86c3c3d6ad168d27346e389dc49
Fernando Martinez <fernando817mm@gmail.com>
2022-01-30 15:42:48 -0600

    Photography page complete

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,6 +3,12 @@ import StyledLandingPage from "../styled-components/StyledLandingPage";
 import StyledContainer from "../styled-components/StyledContainer";
 import Wave from "react-wavify";
 import portrait from "./../images/portrait.jpeg";
+import benito from "./../images/benito.jpeg";
+import droplet from "./../images/droplet.jpeg";
+import hike from "./../images/hike.jpeg";
+import mexico from "./../images/mexico.jpeg";
+import tree from "./../images/tree.jpeg";
+import squat from "./../images/squat.svg";
 
 const LandingPage = () => {
   return (
@@ -50,32 +56,149 @@ const LandingPage = () => {
           scroll for more information<div className="scroll-down"></div>
         </p>
       </StyledContainer>
-      <div className="info two" id="about">
-        <section className="aboutContainer">
-          <div className="aboutTitle">
-            <h1>About me</h1>
-            <h2>
-              <span>¡Hola! I'm Fernando Mauricio Martinez</span> (he/him) - a
-              Texas-born, first-generation Mexican-American Full-Stack Developer
-            </h2>
-          </div>
-          <div className="aboutParagraph">
-            <img
-              src={portrait}
-              alt="Fernando with Colorado mountains in the background."
-            />
-            <p>
-              After graduating high school with a STEM endorsement, I attended
-              and graduated Bloom Institute of Technology, formerly known as
-              Lambda School. Reinforcing and adding new skills to my arsenal.
-              Skills such as (in no specific order); HTML, CSS, Javascript,
-              React, Redux, Git, Less, Node, SQL, Python, etc.
-            </p>
-          </div>
-        </section>
+      <div className="bg">
+        <Wave
+          fill="rgba(255, 186, 1, 0.8)"
+          paused={false}
+          options={{
+            height: 60,
+            amplitude: 60,
+            speed: 0.15,
+            points: 4,
+          }}
+          className="about-background"
+        />
+        <Wave
+          fill="rgba(252, 222, 103, 0.8)"
+          paused={false}
+          options={{
+            height: 70,
+            amplitude: 40,
+            speed: 0.25,
+            points: 3,
+          }}
+          className="about-background"
+        />
+        <Wave
+          fill="rgba(250, 243, 192, 0.8)"
+          paused={false}
+          options={{
+            height: 120,
+            amplitude: 40,
+            speed: 0.25,
+            points: 5,
+          }}
+          className="about-background"
+        />
+        <div className="info two" id="about">
+          <section className="aboutContainer">
+            <div className="aboutTitle">
+              <h1>About me</h1>
+              <h2>
+                <span>¡Hola! I'm Fernando Mauricio Martinez</span> (he/him) - a
+                Texas-born, first-generation Mexican-American Full-Stack
+                Developer
+              </h2>
+            </div>
+            <div className="aboutParagraph">
+              <img
+                src={portrait}
+                alt="Fernando with Colorado mountains in the background."
+              />
+              <p>
+                After graduating high school with a STEM endorsement, I attended
+                and graduated Bloom Institute of Technology, formerly known as
+                Lambda School. Reinforcing and adding new skills to my arsenal.
+                Skills such as (in no specific order); HTML, CSS, Javascript,
+                React, Redux, Git, Less, Node, SQL, Python, etc.
+              </p>
+            </div>
+          </section>
+        </div>
       </div>
-      <div className="info three" id="projects">
-        <h1>Projects</h1>
+      <div className="bg">
+        <Wave
+          fill="rgba(34, 116, 165, 0.8)"
+          paused={false}
+          options={{
+            height: 60,
+            amplitude: 60,
+            speed: 0.15,
+            points: 4,
+          }}
+          className="photo-background"
+        />
+        <Wave
+          fill="rgba(91, 204, 246, 0.8)"
+          paused={false}
+          options={{
+            height: 70,
+            amplitude: 40,
+            speed: 0.25,
+            points: 3,
+          }}
+          className="photo-background"
+        />
+        <Wave
+          fill="rgba(233, 241, 247, 0.8)"
+          paused={false}
+          options={{
+            height: 120,
+            amplitude: 40,
+            speed: 0.25,
+            points: 5,
+          }}
+          className="photo-background"
+        />
+        <div className="info three" id="projects">
+          <section className="photoContainer">
+            <div className="photoParagraph">
+              <h1>Photography</h1>
+              <p>
+                When I'm not coding, I like to spend as much time as possible
+                outside, exploring with my camera & dog (<span>Benito</span>). I
+                shoot on a <span>Canon EOS Rebel T4i</span>, and sometimes just
+                on my phone.
+              </p>
+            </div>
+            <div className="photoTitle">
+              <div className="image-row">
+                <img
+                  className="image image-01"
+                  src={benito}
+                  alt="Happy dog laying on ground facing up"
+                />
+                <img
+                  className="image image-02"
+                  src={droplet}
+                  alt="Water droplet on tip of branch with blurry background"
+                />
+                <img
+                  className="image image-06"
+                  src={squat}
+                  alt="Male character svg"
+                />
+              </div>
+              <div className="image-row">
+                <img
+                  className="image image-03"
+                  src={hike}
+                  alt="Dog on hike with harness looking away from camera. Large hills in background"
+                />
+                <img
+                  className="image image-04"
+                  src={mexico}
+                  alt="Sidewalk in Mexico with graffiti"
+                />
+                <img
+                  className="image image-05"
+                  src={tree}
+                  alt="Sun rays shining through leaf-less tree in the winter"
+                />
+              </div>
+            </div>
+          </section>
+        </div>
       </div>
       <div className="info four" id="contact">
         <h1>Contact</h1>
